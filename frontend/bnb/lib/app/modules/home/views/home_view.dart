a66fd1d95e3a8c6165d99ef65e1f3a169fe7ac48
AlexisDougherty13 <alexisdougherty@ufl.edu>
2022-01-29 00:23:57 -0500

    Played with and started on editing UI

--- frontend/bnb/lib/app/modules/home/views/home_view.dart
+++ frontend/bnb/lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart'; 
+import 'package:get/get.dart';
 import 'package:bnb/app/modules/home/controllers/home_controller.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(239, 222, 205, 0),
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: Text('BnB', style: TextStyle(color: Colors.white, fontSize: 28)),
         centerTitle: true,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/joseph_detail.jpg'), fit: BoxFit.fill)),
+        ),
       ),
       body: Center(
         child: Text(
-          'HomeView is working', 
-          style: TextStyle(fontSize:20),
+          'HomeView is working :-)',
+          //style: TextStyle(fontSize:20),
+          style: GoogleFonts.medievalSharp(
+            textStyle: TextStyle(color: Colors.black, letterSpacing: .5, fontSize: 20),
+          ),
         ),
       ),
     );
   }
 }
-  
